14234caf3c0f15eed5038f1dd3365f2c7d033aae
DeepRoychoudhury <deeproydrc@gmail.com>
2021-05-13 16:39:25 +0100

    Added images and modified css

--- src/component/Leftfooter.js
+++ src/component/Leftfooter.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
 import './Leftfooter.css';
+import terminal from '../images/terminal.png';
+import options from '../images/options.JPG';
+import mozilla from '../images/mozilla.jpg';
+import libreofficewriter from '../images/LibreOfficeWriter.JPG';
+import folder from '../images/folder.JPG';
 
 class Leftfooter extends Component {
+	handleHover(x){
+		console.log("mouseover");
+	}
+
+	exitHover(){
+		console.log("mouseout");
+	}
+
 	render(){
 		return(
 				<div class="leftfoot">
-						<a href="blank">Home</a>
-						<a href="blank">File Explorer</a>
-						<a href="blank">Browser</a>
-						<a href="blank">Terminal</a>
+						<a href="blank" onMouseEnter={this.handleHover(this)} onMouseLeave={this.exitHover}>
+							<img src={terminal} alt="terminal"/>
+						</a>
+						<a href="blank">
+							<img src={mozilla} alt="terminal"/>
+						</a>
+						<a href="blank">
+							<img src={folder} alt="terminal"/>
+						</a>
+						<a href="blank">
+							<img src={libreofficewriter} alt="terminal"/>
+						</a>
+						<a href="blank" class="options">
+							<img src={options} alt="terminal"/>
+						</a>
 				</div>
 			)
 	}
